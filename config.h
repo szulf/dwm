4b08b3267032466ed6ffd272c143b3aff816056e
szulf <139226203+szulf@users.noreply.github.com>
2025-06-20 00:01:20 +0200

    my dwm config idk

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int gappx = 6;    /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
@@ -19,7 +20,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {"1", "2", "3", "4", "5", "6"};
 
 static const Rule rules[] = {
   /* xprop(1):
@@ -27,14 +28,12 @@ static const Rule rules[] = {
    *	WM_NAME(STRING) = title
    */
   /* class      instance    title       tags mask     isfloating   monitor */
-  {"Gimp",    NULL, NULL, 0,      1, -1},
-  {"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
 
 /* layout(s) */
 static const float mfact = 0.55;     /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;        /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -45,7 +44,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY, TAG)                                                                          \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                                           \
     {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},                                     \
@@ -69,33 +68,40 @@ static const char *dmenucmd[] = {"dmenu_run", "-m",      dmenumon,  "-fn",     d
 static const char *termcmd[] = {"st", NULL};
 
 static const Key keys[] = {
-  /* modifier                     key        function        argument */
-  {MODKEY,             XK_p,      spawn,          {.v = dmenucmd}   },
-  {MODKEY | ShiftMask, XK_Return, spawn,          {.v = termcmd}    },
-  {MODKEY,             XK_b,      togglebar,      {0}               },
-  {MODKEY,             XK_j,      focusstack,     {.i = +1}         },
-  {MODKEY,             XK_k,      focusstack,     {.i = -1}         },
-  {MODKEY,             XK_i,      incnmaster,     {.i = +1}         },
-  {MODKEY,             XK_d,      incnmaster,     {.i = -1}         },
-  {MODKEY,             XK_h,      setmfact,       {.f = -0.05}      },
-  {MODKEY,             XK_l,      setmfact,       {.f = +0.05}      },
-  {MODKEY,             XK_Return, zoom,           {0}               },
-  {MODKEY,             XK_Tab,    view,           {0}               },
-  {MODKEY | ShiftMask, XK_c,      killclient,     {0}               },
-  {MODKEY,             XK_t,      setlayout,      {.v = &layouts[0]}},
-  {MODKEY,             XK_f,      setlayout,      {.v = &layouts[1]}},
-  {MODKEY,             XK_m,      setlayout,      {.v = &layouts[2]}},
-  {MODKEY,             XK_space,  setlayout,      {0}               },
-  {MODKEY | ShiftMask, XK_space,  togglefloating, {0}               },
-  {MODKEY,             XK_0,      view,           {.ui = ~0}        },
-  {MODKEY | ShiftMask, XK_0,      tag,            {.ui = ~0}        },
-  {MODKEY,             XK_comma,  focusmon,       {.i = -1}         },
-  {MODKEY,             XK_period, focusmon,       {.i = +1}         },
-  {MODKEY | ShiftMask, XK_comma,  tagmon,         {.i = -1}         },
-  {MODKEY | ShiftMask, XK_period, tagmon,         {.i = +1}         },
-  TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4)
-    TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-      TAGKEYS(XK_9, 8) {MODKEY | ShiftMask, XK_q,      quit,           {0}               },
+  /* modifier                     key           function        argument */
+  {MODKEY,             XK_p,         spawn,          {.v = dmenucmd}   },
+  {MODKEY,             XK_Return,    spawn,          {.v = termcmd}    },
+  {MODKEY,             XK_b,         togglebar,      {0}               },
+  {MODKEY,             XK_j,         focusstack,     {.i = +1}         },
+  {MODKEY,             XK_k,         focusstack,     {.i = -1}         },
+  {MODKEY,             XK_i,         incnmaster,     {.i = +1}         },
+  {MODKEY,             XK_d,         incnmaster,     {.i = -1}         },
+  {MODKEY,             XK_h,         setmfact,       {.f = -0.05}      },
+  {MODKEY,             XK_l,         setmfact,       {.f = +0.05}      },
+  {MODKEY,             XK_semicolon, zoom,           {0}               },
+  {MODKEY,             XK_Tab,       view,           {0}               },
+  {MODKEY | ShiftMask, XK_q,         killclient,     {0}               },
+  {MODKEY,             XK_t,         setlayout,      {.v = &layouts[0]}},
+  {MODKEY,             XK_f,         setlayout,      {.v = &layouts[1]}},
+  {MODKEY,             XK_m,         setlayout,      {.v = &layouts[2]}},
+  {MODKEY,             XK_space,     setlayout,      {0}               },
+  {MODKEY | ShiftMask, XK_space,     togglefloating, {0}               },
+  {MODKEY,             XK_0,         view,           {.ui = ~0}        },
+  {MODKEY | ShiftMask, XK_0,         tag,            {.ui = ~0}        },
+  {MODKEY,             XK_comma,     focusmon,       {.i = -1}         },
+  {MODKEY,             XK_period,    focusmon,       {.i = +1}         },
+  {MODKEY | ShiftMask, XK_comma,     tagmon,         {.i = -1}         },
+  {MODKEY | ShiftMask, XK_period,    tagmon,         {.i = +1}         },
+  {MODKEY | ShiftMask, XK_x,         quit,           {0}               },
+  TAGKEYS(XK_1, 0),
+  TAGKEYS(XK_2, 1),
+  TAGKEYS(XK_3, 2),
+  TAGKEYS(XK_4, 3),
+  TAGKEYS(XK_5, 4),
+  TAGKEYS(XK_6, 5),
+  TAGKEYS(XK_7, 6),
+  TAGKEYS(XK_8, 7),
+  TAGKEYS(XK_9, 8),
 };
 
 /* button definitions */
@@ -114,3 +120,5 @@ static const Button buttons[] = {
   {ClkTagBar,     MODKEY, Button1, tag,            {0}               },
   {ClkTagBar,     MODKEY, Button3, toggletag,      {0}               },
 };
+
+static const char *usercmds[] = {"xsetroot -solid \"#000000\"", "dwmblocks &"};
