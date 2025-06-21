781841f3b41b93cdb15e886a9443de9ae1bad437
szulf <139226203+szulf@users.noreply.github.com>
2025-06-21 11:15:15 +0200

    rules fix?

--- config.h
+++ config.h
@@ -28,6 +28,7 @@ static const Rule rules[] = {
    *	WM_NAME(STRING) = title
    */
   /* class      instance    title       tags mask     isfloating   monitor */
+  {0, 0, 0, 0, 0, 0}
 };
 
 /* layout(s) */
@@ -93,15 +94,8 @@ static const Key keys[] = {
   {MODKEY | ShiftMask, XK_comma,     tagmon,         {.i = -1}         },
   {MODKEY | ShiftMask, XK_period,    tagmon,         {.i = +1}         },
   {MODKEY | ShiftMask, XK_x,         quit,           {0}               },
-  TAGKEYS(XK_1, 0),
-  TAGKEYS(XK_2, 1),
-  TAGKEYS(XK_3, 2),
-  TAGKEYS(XK_4, 3),
-  TAGKEYS(XK_5, 4),
-  TAGKEYS(XK_6, 5),
-  TAGKEYS(XK_7, 6),
-  TAGKEYS(XK_8, 7),
-  TAGKEYS(XK_9, 8),
+  TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4)
+    TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
 };
 
 /* button definitions */
